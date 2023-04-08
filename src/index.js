3cae0d084bb0c1351208d5a690e423382f42f7bb
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2023-04-08 08:19:05 +0300

    add  a function that iterates over the tasks array and populates the HTML list item element for each task and an array of some simple to do tasks

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
-import './style.css';
+import "./style.css";
 import {
-  render, remove, markAsCompleted, editTask, cleareCompleted, tasks, formaction,
-} from './script.js';
+  render,
+  remove,
+  markAsCompleted,
+  editTask,
+  cleareCompleted,
+  tasks,
+  formaction,
+} from "./script.js";
 
-const toDoListContainer = document.querySelector('.todo-list');
+const toDoListContainer = document.querySelector(".todo-list");
 
 formaction();
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   tasks.sort((a, b) => a.index - b.index).map((item) => render(item));
   remove(toDoListContainer);
   tasks.forEach((task) => {
     if (task.completed) {
       const taskIndex = task.index;
-      toDoListContainer.querySelectorAll('.text').forEach((text) => {
-        const textParentNodeId = parseInt(text.parentNode.parentNode.parentNode.id, 10);
+      toDoListContainer.querySelectorAll(".text").forEach((text) => {
+        const textParentNodeId = parseInt(
+          text.parentNode.parentNode.parentNode.id,
+          10
+        );
         if (textParentNodeId === taskIndex) {
-          text.classList.add('completed');
+          text.classList.add("completed");
         }
       });
     }
--- src/script.js
+++ src/script.js
@@ -1,140 +1,78 @@
-import toggleComplete from './complete.js'; // eslint-disable-line 
+// Define the task array
+let tasks = [];
 
-const completeBtn = document.querySelector('.complete');
-const toDoListContainer = document.querySelector('.todo-list');
-const form = document.querySelector('form');
-
-let tasks = []; // eslint-disable-line 
-
-export const updateStorage = (task) => {
-  localStorage.setItem('My-To-Do-List', JSON.stringify(task));
-};
-
-// render newly added task to page --------
-export const render = (task) => {
-  toDoListContainer.innerHTML += `
-  <div id="${task.index}">
-  <div class="list">
-    <div class="list-description">
-      <input type="checkbox" class="checkbox">
-      <p contenteditable="true" class="text" spellcheck="false">${task.description}</p>
-    </div>
-    <div class="icon-container">
-      <i class="fa-solid fa-ellipsis-vertical icon"></i>
-      <i class="fa-regular fa-trash-can delete active"></i>
-    </div>
-  </div>
-  <hr>
-</div>
-  `;
-};
-
-// Remove selected task from list---
-const removeId = (id) => {
-  tasks = tasks.filter((task) => task.index !== id);
-  for (let i = 0; i < tasks.length; i += 1) {
-    tasks[i].index = i;
-    updateStorage(tasks);
-  }
-  updateStorage(tasks);
-};
-
-export const remove = (element) => {
-  element.querySelectorAll('.icon').forEach((icon) => {
-    icon.addEventListener('click', (e) => {
-      const parent = e.target.parentNode.parentNode.parentNode;
-      icon.classList.add('active');
-      icon.nextElementSibling.classList.remove('active');
-      setTimeout(() => {
-        icon.nextElementSibling.classList.add('active');
-        icon.classList.remove('active');
-      }, 4000);
-      element.querySelectorAll('.delete').forEach((btn) => {
-        btn.addEventListener('click', () => {
-          removeId(parseInt(parent.id, 10));
-          parent.remove();
-        });
-      });
-    });
-  });
-};
-
-export const markAsCompleted = (element) => {
-  element.querySelectorAll('.checkbox').forEach((box) => {
-    box.addEventListener('change', () => {
-      const item = box.nextElementSibling;
-      toggleComplete(item);
-      updateStorage(tasks);
-    });
-  });
-};
-
-// Cleare completed files---------
-export const cleareCompleted = (element) => {
-  completeBtn.addEventListener('click', () => {
-    element.querySelectorAll('.list').forEach((list) => {
-      const taskId = parseInt(list.parentNode.id, 10);
-      const parentnode = list.parentNode;
-      tasks.forEach((task) => {
-        if (task.completed && task.index === taskId) {
-          parentnode.remove();
-        }
-      });
-    });
-    tasks = tasks.filter((task) => task.completed !== true);
-    for (let i = 0; i < tasks.length; i += 1) {
-      tasks[i].index = i;
-      updateStorage(tasks);
-    }
-    updateStorage(tasks);
-  });
-};
-
-// // edit task----------------
-export const editTask = (element) => {
-  element.querySelectorAll('.text').forEach((box) => {
-    box.addEventListener('input', (e) => {
-      const activeText = e.target;
-      const textContent = activeText.innerHTML;
-      const activeTextParentId = parseInt(activeText.parentNode.parentNode.parentNode.id, 10);
-      tasks.forEach((task) => {
-        if (task.index === activeTextParentId) {
-          task.description = textContent;
-          updateStorage(tasks);
-        }
-      });
-    });
-  });
-};
-
-//   // Add new task to the list---------
-const add = (task) => {
-  render(task);
-  tasks.push(task);
-  updateStorage(tasks);
-  remove(toDoListContainer);
-  editTask(toDoListContainer);
-  markAsCompleted(toDoListContainer);
-  cleareCompleted(toDoListContainer);
-};
-
-export const formaction = () => {
-  form.onsubmit = (e) => {
-    e.preventDefault();
-    const { text } = e.target;
-    add({
-      description: text.value,
-      completed: false,
-      index: tasks.length,
-    });
-    text.value = '';
+// Define the function to add new task
+const addTask = (description) => {
+  const task = {
+    description,
+    completed: false,
+    index: tasks.length,
   };
+  tasks.push(task);
+  return task;
 };
 
-if (localStorage.getItem('My-To-Do-List')) {
-  tasks = JSON.parse(localStorage.getItem('My-To-Do-List'));
-} else {
-  localStorage.setItem('My-To-Do-List', JSON.stringify([]));
-}
+// Define the function to toggle task completion status
+const toggleTaskCompletion = (id) => {
+  const task = tasks[id];
+  task.completed = !task.completed;
+  return task;
+};
 
-export { tasks };
+// Define the function to remove a task
+const removeTask = (id) => {
+  tasks.splice(id, 1);
+};
+
+// Define the function to populate the task list
+const populateTasks = () => {
+  const parentElement = document.getElementById("taskList");
+  parentElement.innerHTML = "";
+  tasks.forEach((task) => {
+    const { description, completed, index: id } = task;
+    const innerHTML = `
+      <p>${description}</p>
+      <p>${completed ? "completed" : "not completed"}</p>
+    `;
+    const newTodoListElement = document.createElement("li");
+    Object.assign(newTodoListElement, { innerHTML, id });
+    parentElement.appendChild(newTodoListElement);
+  });
+};
+
+// Add event listener for form submission
+const form = document.getElementById("addTaskForm");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const input = document.getElementById("newTaskInput");
+  const description = input.value.trim();
+  if (description) {
+    addTask(description);
+    input.value = "";
+    populateTasks();
+  }
+});
+
+// Add event listener for task completion checkbox
+const taskList = document.getElementById("taskList");
+taskList.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.type === "checkbox") {
+    const id = Number(target.parentElement.id);
+    toggleTaskCompletion(id);
+    populateTasks();
+  }
+});
+
+// Add event listener for remove button
+taskList.addEventListener("click", (event) => {
+  const target = event.target;
+  if (target.classList.contains("removeButton")) {
+    const id = Number(target.parentElement.id);
+    removeTask(id);
+    populateTasks();
+  }
+});
+
+// Initial population of task list
+populateTasks();
