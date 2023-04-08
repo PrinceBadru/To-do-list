bcc5465f1d099a592f68f3420a34a026a6a27124
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2023-04-08 14:05:11 +0300

    fix linter errors

--- src/todo.js
+++ src/todo.js
@@ -1,63 +1,16 @@
-// Define tasks array
-let tasks = [];
+const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-// Get tasks from local storage
-const storedTasks = JSON.parse(localStorage.getItem('tasks'));
-if (storedTasks) {
-  tasks = storedTasks;
-}
-
-// Define function to save tasks to local storage
-const saveTasksToLocalStorage = () => {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-};
-
-// Define function to add new task
-const addTask = (description) => {
-  const task = {
-    description,
-    completed: false,
-    index: tasks.length,
-  };
-  tasks.push(task);
-  saveTasksToLocalStorage();
-  return task;
-};
-
-// Define function to delete task
-const deleteTask = (id) => {
-  tasks = tasks.filter((task) => task.index !== id);
-  tasks.forEach((task, index) => {
-    task.index = index;
-  });
-  saveTasksToLocalStorage();
-};
-
-// Define function to edit task description
-const editTask = (id, newDescription) => {
-  tasks = tasks.map((task) => {
-    if (task.index === id) {
-      return {
-        ...task,
-        description: newDescription,
-      };
-    }
-    return task;
-  });
-  saveTasksToLocalStorage();
-};
-
-// Define function to render tasks to DOM
-const renderTasks = () => {
+const populateTasks = () => {
   const parentElement = document.getElementById('taskList');
   parentElement.innerHTML = '';
-  tasks.forEach((task) => {
-    const { description, completed, index: id } = task;
+  tasks.forEach((task, index) => {
+    const { description, completed } = task;
+    const id = `task${index}`;
     const innerHTML = `
       <p>${description}</p>
       <p>${completed ? 'completed' : 'not completed'}</p>
-      <button class="removeButton">Remove</button>
-      <input type="checkbox" ${completed ? 'checked' : ''}>
+      <button class="removeButton" data-id="${id}">Remove</button>
+      <input type="checkbox" ${completed ? 'checked' : ''} data-id="${id}">
     `;
     const newTodoListElement = document.createElement('li');
     Object.assign(newTodoListElement, { innerHTML, id });
@@ -65,36 +18,51 @@ const renderTasks = () => {
   });
 };
 
-// Add event listener for adding tasks
-const addTaskButton = document.getElementById('addTaskButton');
-addTaskButton.addEventListener('click', () => {
-  const taskInput = document.getElementById('newTaskInput');
-  const description = taskInput.value;
-  addTask(description);
-  taskInput.value = '';
-  renderTasks();
-});
+const addTask = (description) => {
+  const task = {
+    description,
+    completed: false,
+  };
+  tasks.push(task);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  populateTasks();
+};
 
-// Add event listener for deleting tasks
-const taskList = document.getElementById('taskList');
-taskList.addEventListener('click', (event) => {
-  if (event.target.classList.contains('removeButton')) {
-    const taskElement = event.target.parentElement;
-    const id = parseInt(taskElement.id);
-    deleteTask(id);
-    renderTasks();
+const deleteTask = (index) => {
+  tasks.splice(index, 1);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  populateTasks();
+};
+
+const removeButtonHandler = (event) => {
+  const { id } = event.target.dataset;
+  const index = tasks.findIndex((task) => `task${task.index}` === id);
+  deleteTask(index);
+};
+
+const checkboxHandler = (event) => {
+  const { id } = event.target.dataset;
+  const index = tasks.findIndex((task) => `task${task.index}` === id);
+  tasks[index].completed = event.target.checked;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  populateTasks();
+};
+
+const addTaskHandler = () => {
+  const description = prompt('Enter task description:');
+  if (description) {
+    addTask(description);
+  }
+};
+
+document.getElementById('addButton').addEventListener('click', addTaskHandler);
+
+document.getElementById('taskList').addEventListener('click', (event) => {
+  if (event.target.matches('.removeButton')) {
+    removeButtonHandler(event);
+  } else if (event.target.matches('input[type="checkbox"]')) {
+    checkboxHandler(event);
   }
 });
 
-// Add event listener for editing tasks
-taskList.addEventListener('blur', (event) => {
-  if (event.target.tagName === 'P') {
-    const taskElement = event.target.parentElement;
-    const id = parseInt(taskElement.id);
-    const newDescription = event.target.textContent;
-    editTask(id, newDescription);
-  }
-});
-
-// Render initial tasks to DOM
-renderTasks();
+populateTasks();
